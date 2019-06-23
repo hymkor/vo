e644d7809108491d801934df87fb515f7bec1ae5
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-06-24 01:42:55 +0900

    Move code to get time-stamp to pe-stamp.go

--- list.go
+++ list.go
@@ -139,49 +139,6 @@ func listProductInline(sln *Solution) error {
 	return nil
 }
 
-const FILE_ADDRESS_OF_NEW_EXE_HEADER = 60
-
-func bytes2dword(array []byte) int64 {
-	return int64(array[0]) +
-		int64(array[1])*256 +
-		int64(array[2])*256*256 +
-		int64(array[3])*256*256*256
-}
-
-func GetPeHeaderPos(fd io.ReadSeeker) (int64, error) {
-	var array [4]byte
-
-	_, err := fd.Seek(FILE_ADDRESS_OF_NEW_EXE_HEADER, io.SeekStart)
-	if err != nil {
-		return 0, err
-	}
-	_, err = fd.Read(array[:])
-	if err != nil {
-		return 0, err
-	}
-	return bytes2dword(array[:]), nil
-}
-
-func getExeStamp(fd io.ReadSeeker) (time.Time, error) {
-	var array [4]byte
-
-	peHeaderPos, err := GetPeHeaderPos(fd)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	_, err = fd.Seek(peHeaderPos+8, io.SeekStart)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	_, err = fd.Read(array[:])
-	if err != nil {
-		return time.Time{}, err
-	}
-	return time.Unix(bytes2dword(array[:]), 0), nil
-}
-
 type exeSpec struct {
 	Name           string
 	Md5Sum         string
@@ -219,7 +176,7 @@ func getExeSpec(fname string) *exeSpec {
 		return nil
 	}
 
-	stamp, _ := getExeStamp(fd)
+	stamp, _ := GetPEStamp(fd)
 
 	return &exeSpec{
 		Name:           fname,
